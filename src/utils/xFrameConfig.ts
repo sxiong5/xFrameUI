52647244c1a478eeb6598457eae9b237d7831d33
ShihaoXiong <shihao.xiong@icloud.com>
2021-06-22 16:19:25 +0800

    Core: Add the configuration

--- src/utils/xFrameConfig.ts
+++ src/utils/xFrameConfig.ts
@@ -1,1 +1,9 @@
 export type xFrameComponentSize = 'small' | 'middle' | 'large';
+
+export type xFrameTheme = '' | '' | '' | '';
+
+export interface ButtonThemeOptions {
+	background: string;
+	color: string;
+	activeColor?: string;
+}
